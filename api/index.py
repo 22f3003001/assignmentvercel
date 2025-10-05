687a7f51bf103d77e5c9ec3fd563d7a066cc23e6
22f3003001 <22f3003001@ds.study.iitm.ac.in>
2025-10-05 21:56:05 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,15 +7,14 @@ import os
 
 app = FastAPI()
 
-# Enable CORS for all origins (POST requests)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],           # allow all origins
-    allow_credentials=True,
-    allow_methods=["*"],           # allow all methods (includes OPTIONS)
-    allow_headers=["*"],           # allow all headers
+    allow_origins=["*"],
+    allow_credentials=False,  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
 BASE_DIR = os.path.dirname(__file__)
 json_path = os.path.join(BASE_DIR, "q-vercel-latency.json")
 
